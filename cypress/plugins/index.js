5baad5f5fe7c9a5c0d31d89253879c1ae4278280
Tarek Mohsen <mohsen.tarek@gmail.com>
2021-02-20 12:48:10 +0200

    Fix lint issues regarding cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+/*eslint no-unused-vars: ["error", { "args": "none" }]*/
+
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
